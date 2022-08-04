2272268cfe8d3c51a87b98fde8f69a117dcc9f34
The Coding Lemon <twitter.com/codewithlemon>
2022-08-05 01:45:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Twitter User Profile Finder
+
+This is a REST API that gets user profiles by their Twitter ID or username.
+
+It requires a Twitter developer access token to be defined in the environment
+to get access to Twitter APIs.
+
+The project is based on Node.js and the Express framework.
+
+
+## Instructions
+
+To setup this project, enter the following commands:
+
+```sh
+git clone https://github.com/codewithlemon/twitter-user-profile-finder
+cd twitter-user-profile-finder
+npm install
+```
+
+Create a file called `.env` and paste in your Twitter developer access token.
+
+```
+TWITTER_ACCESS_TOKEN=...
+```
+
+Run the project in development mode.
+
+```sh
+npm run dev
+```
+
+Alternatively, you can run the project in production mode.
+
+```sh
+TWITTER_ACCESS_TOKEN=... npm run start
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+if (process.env.NODE_ENV === "development") require("dotenv").config()
+
+const fetch = require("node-fetch")
+const express = require("express")
+const cors = require("cors")
+const port = process.env.PORT || 5000
+
+const twitter_access_token = process.env.TWITTER_ACCESS_TOKEN
+
+if (!twitter_access_token)
+  throw new Error("Twitter access token is not defined in the environment.")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/profile/by-id/:userId", async (req, res) => {
+  const userId = req.params.userId
+
+  if (!userId)
+    res.status(400).json({
+      error: "Missing or invalid user ID",
+    })
+
+  const response = await fetch(`https://api.twitter.com/2/users/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${twitter_access_token}`,
+    },
+  })
+
+  const data = await response.json()
+
+  if (data.data) {
+    res.json({
+      message: "User found",
+      user: data,
+    })
+  } else {
+    res.json({
+      message: "No such user",
+    })
+  }
+})
+
+app.get("/profile/by-username/:username", async (req, res) => {
+  const username = req.params.username
+
+  if (!username)
+    res.status(400).json({
+      error: "Missing or invalid username",
+    })
+
+  const response = await fetch(
+    `https://api.twitter.com/2/users/by/username/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${twitter_access_token}`,
+      },
+    }
+  )
+
+  const data = await response.json()
+
+  if (data.data) {
+    res.json({
+      message: "User found",
+      user: data,
+    })
+  } else {
+    res.json({
+      message: "No such user",
+    })
+  }
+})
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "No such resource",
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port} ...`)
+})
